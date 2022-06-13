897f301fa36b956693b63f239b95fc2fb50ba790
Wandia E Mugo <wwandia173@gmail.com>
2022-06-13 23:03:21 +0300

    commit tree fe1a7dd0b620b8ad843a9906f8c99f7dff9cb141

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("This is really cool!");
+    newDom()
+});
+
+function newDom(){
+    document.getElementById("text").innerHTML= "This is really cool!";
+}
